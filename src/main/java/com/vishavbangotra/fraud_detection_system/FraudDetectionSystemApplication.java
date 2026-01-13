da551b51037284920858bc2cd5df6c772d78f7a4
Vishav Bangotra <44341523+vishavbangotra@users.noreply.github.com>
2026-01-13 19:13:20 +0530

    Initial commit

---
+++ src/main/java/com/vishavbangotra/fraud_detection_system/FraudDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vishavbangotra.fraud_detection_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudDetectionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vishavbangotra/fraud_detection_system/FraudDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishavbangotra.fraud_detection_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
